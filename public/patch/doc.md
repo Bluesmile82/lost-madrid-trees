678e8c23a5ffe243f13bf84263b0fb95ae1cfd22
Alvaro Leal <pursuance@gmail.com>
2023-03-25 01:25:03 +0100

    Add tailwind and play time

--- public/patch/doc.md
+++ public/patch/doc.md
@@ -1,5 +1,6 @@
 ## Patch Variables:
 
+* __dolly__ ```Number```
 * __page1Visibility__ ```Number```
 * __year__ ```Number```
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,51 @@ import CablesPatch from './CablesPatch';
 import './App.css';
 import { useEffect, useState, useCallback } from 'react';
 
+const YearSelector = ({ years, setYear, selectedYear }) => (
+  <div className="relative flex mt-6">
+    <span className="absolute w-full top-1.5 border border-t-1.5 border-black" />
+    {years.map((year) => (
+      <div className="relative flex flex-col items-center">
+        <span
+          className={`h-4 w-4 bg-white rounded-full ${
+            selectedYear === year && 'border-2 border-black'
+          }`}
+        />
+        <span
+          className="year relative ml-2"
+          onMouseEnter={(e) => {
+            e.preventDefault();
+            setYear(year);
+          }}
+        >
+          {year}
+        </span>
+      </div>
+    ))}
+  </div>
+);
 function App() {
   const [variables, setVariables] = useState();
   const [CABLES, setCABLES] = useState();
+  const years = [2019, 2021, 2022];
+  const [year, setYear] = useState(years[0]);
+
+  useEffect(() => {
+    if (CABLES) {
+      CABLES.patch.setVariable('year', year);
+    }
+  }, [year, CABLES]);
+
   const [selectedVariables, setSelectedVariables] = useState({});
+
+  function handlePlay() {
+    setYear(years[0]);
+    for (let i = 0; i < years.length; i++) {
+      setTimeout(() => {
+        setYear(years[i]);
+      }, 800 * i);
+    }
+  }
 
   const setSelectedVariable = useCallback(
     (key, variable) => {
@@ -26,13 +67,29 @@ function App() {
     }
   }, [variables, setSelectedVariable]);
 
-  const { year, page1Visibility } = selectedVariables || {};
-  console.log(page1Visibility);
+  const { year: yearVariable, page1Visibility } = selectedVariables || {};
+  const renderYears = () => {
+    return (
+      <div className="years">
+        <div className="flex items-center justify-center">
+          <div className="relative w-10 ml-4">
+            <button className="play-button" onClick={handlePlay}>
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path d="M8 5v14l11-7z" />
+              </svg>
+            </button>
+          </div>
+          <YearSelector years={years} setYear={setYear} selectedYear={year} />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <div className="container" style={{ opacity: page1Visibility }}>
         <h1 className="title">Pérdida de árboles en Madrid</h1>
-        <h2 className="title">{year}</h2>
+        <h2 className="title">{yearVariable}</h2>
         <p>
           Madrid ha perdido 78.616 árboles maduros en sus calles y parques en
           los últimos cuatro años.
@@ -46,39 +103,10 @@ function App() {
         patchDir={'/patch/'}
         setVariables={setVariables}
         setCABLES={setCABLES}
+        canvasId={'canvas'}
+        patchOptions={{ glCanvasResizeToWindow: false }}
       />
-      {page1Visibility < 1 && (
-        <div className="years">
-          Pasa el cursor sobre los años:{' '}
-          <span
-            className="year"
-            onMouseEnter={(e) => {
-              e.preventDefault();
-              CABLES.patch.setVariable('year', 2019);
-            }}
-          >
-            2019
-          </span>
-          <span
-            className="year"
-            onMouseEnter={(e) => {
-              e.preventDefault();
-              CABLES.patch.setVariable('year', 2021);
-            }}
-          >
-            2021
-          </span>
-          <span
-            className="year"
-            onMouseEnter={(e) => {
-              e.preventDefault();
-              CABLES.patch.setVariable('year', 2022);
-            }}
-          >
-            2022
-          </span>
-        </div>
-      )}
+      {page1Visibility < 1 && renderYears()}
     </div>
   );
 }
